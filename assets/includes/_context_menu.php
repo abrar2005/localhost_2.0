0cf0d865be2ec7276e3f84b4d2967f9a3b5ac83a
Abrar-Muftah <abrarmuftah1@gmail.com>
2022-07-20 20:23:29 +0200

    update

--- assets/includes/_context_menu.php
+++ assets/includes/_context_menu.php
@@ -1,6 +1,24 @@
 <div class="cm__main">
     <ul>
-        <li>
+        <div class="webActions">
+            <div class="cm__item">
+                <img src="assets/img/context_menu/arrow.png" alt="" class="cm__item_img">
+                <p class="cm__text" onclick="window.history.back()">Back</p>
+            </div>
+            <div class="cm__item">
+                <img style="transform: rotate(180deg)" src="assets/img/context_menu/arrow.png" alt="" class="cm__item_img">
+                <p class="cm__text" onclick="window.history.forward()">Forward</p>
+            </div>
+            
+            <div class="divider"></div>
+            <div class="cm__item">
+                <img src="assets/img/context_menu/refresh.png" alt="" class="cm__item_img">
+                <a href="" class="cm__text">Reload</a>
+            </div>
+        </div>
+
+
+        <div class="fileActions">
             <div class="cm__item">
                 <img src="assets/img/context_menu/copy.png" alt="" class="cm__item_img">
                 <a href="" class="cm__text">Copy</a>
@@ -13,11 +31,7 @@
                 <img src="assets/img/context_menu/delete.png" alt="" class="cm__item_img">
                 <a href="" class="cm__text">Delete</a>
             </div>
-            <div class="divider"></div>
-            <div class="cm__item">
-                <img src="assets/img/context_menu/refresh.png" alt="" class="cm__item_img">
-                <a href="" class="cm__text">Reload</a>
-            </div>
-        </li>
+        </div>
+
     </ul>
 </div>
--- assets/js/main.js
+++ assets/js/main.js
@@ -45,8 +45,24 @@ allAnchors.forEach((e) => {
 		customContextMenu.style.left = leftPosition + "px";
 		customContextMenu.style.top = topPosition + "px";
 
+		document.querySelector(".fileActions").style.display = "block";
+
 		console.log(e);
 	});
+});
+
+document.addEventListener("contextmenu", (e) => {
+	e.preventDefault();
+	let topPosition = e.clientY;
+	let leftPosition = e.clientX;
+	customContextMenu.classList.add("active");
+
+	customContextMenu.style.left = leftPosition + "px";
+	customContextMenu.style.top = topPosition + "px";
+
+	document.querySelector(".fileActions").style.display = "none";
+
+	console.log(e);
 });
 
 window.addEventListener("click", () => {
